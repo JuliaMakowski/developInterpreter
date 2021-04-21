8fe0aac28bf495b5630f3d23a7b8c58c84119340
Julia <juliamakowski@hotmail.com>
2021-04-21 15:51:10 -0300

    create commands cases

--- src/src/VM/VM.java
+++ src/src/VM/VM.java
@@ -12,4 +12,45 @@ public class VM {
         dataPointer=0;
         programPointer=0;
     }
+
+    public void commands(char c){
+        switch (c){
+            case '>':
+                dataPointer++;
+                programPointer++;
+                break;
+            case '<':
+                dataPointer--;
+                programPointer++;
+                break;
+            case '+':
+                memory[dataPointer]++;
+                programPointer++;
+                break;
+            case '-':
+                memory[dataPointer]--;
+                programPointer++;
+                break;
+            case '[':
+                if(dataPointer==0) {//  então desloque o ponteiro de programa para o próximo comando em sequência ao ] correspondente.
+                    // Caso contrário, avance o ponteiro de programa.
+                } else programPointer++;
+                break;
+            case ']':
+                if (dataPointer!=0) // então retroceda o ponteiro de programa para o [ correspondente.
+                break;
+            case ',':
+                //   lê um caractere do arquivo IF e o armazena na posição apontada pelo ponteiro de dados
+                break;
+            case '.':
+                // escreve no arquivo OF o caractere apontado pelo ponteiro de dados.
+                break;
+            case '$':
+                //  termina o programa e imprime o conteúdo da memória na segunda linha do arquivo OF.
+                break;
+            default:
+                programPointer++;
+                break;
+        }
+    }
 }
